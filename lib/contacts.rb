bf79b046bf61dd8ac396fbfa1788d46051ac7814
Halie Hall <halierae17@outlook.com>
2021-04-23 20:43:49 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -19,5 +19,9 @@ def remove_strawberry(contacts)
     if person == "freddy Mercury"
      data.each do |key, value|
       if key == :favorite_ice_cream_flavors
-        
+        value.delete_if {|ice_cream| ice_cream == "strawberry"}
+      end 
+    end
+  end
+ end
 end
